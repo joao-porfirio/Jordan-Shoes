d54e5cabe8db7a6ead53834a273b4b25086a1477
Joao Porfirio <joaoprf54@gmail.com>
2021-09-17 12:55:46 -0300

    Componente Destaques

---
+++ src/Components/Destaques/Destaques.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Produto from '../Produto/Produto'
+import jordan_1 from '../../assets/img/1.png'
+import jordan_2 from '../../assets/img/2.png'
+import jordan_3 from '../../assets/img/3.png'
+import jordan_4 from '../../assets/img/4.png'
+import jordan_5 from '../../assets/img/5.png'
+import jordan_6 from '../../assets/img/6.png'
+import jordan_7 from '../../assets/img/7.png'
+
+const Destaques = () => {
+  return (
+    <article>
+      <h1>Destaques</h1>
+      <p>
+        Frete grátis e chinelo de brinde é aqui, 
+        aproveite por tempo limitado.
+      </p>
+      <Produto src={jordan_1} alt="123"/>
+      <Produto src={jordan_2} alt="123"/>
+      <Produto src={jordan_3} alt="123"/>
+      <Produto src={jordan_4} alt="123"/>
+    </article>
+  )
+}
+
+export default Destaques
