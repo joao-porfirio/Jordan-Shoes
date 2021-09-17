06d60de5f03bf437ee457ad30537c9bfd4858949
Joao Porfirio <joaoprf54@gmail.com>
2021-09-17 17:04:45 -0300

    Ajustando html seção Main

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import logo from '../../assets/img/wallpaper.jpeg';
+import banner from '../../assets/img/wallpaper.jpeg';
 const Main = () => {
   return (
     <section>
-      <main>
-        <img src={logo}/>
-        <h2>A melhor loja de Jordan</h2>
-        <p>O tênis Jordan é fruto de uma velha e forte<br/> 
-        parceria entre a Nike e o jogador Michael Jordan.</p>
+      <main className="main">
+        <article className="main__info">
+          <img src={banner} alt="" className="main__banner" />
+        </article>
+        <aside className="main__banner-content">
+          <h2 className="main__banner-bord">A melhor loja de Jordan</h2>
+          <p className="main__banner-text">O tênis Jordan é fruto de uma velha e forte<br/> 
+          parceria entre a Nike e o jogador Michael Jordan.</p>
+        </aside>
       </main> 
     </section>
   )
