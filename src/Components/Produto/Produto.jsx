f2168da5e809b1f4ec9a6c9d4a8ecc2b177d6e10
Joao Porfirio <joaoprf54@gmail.com>
2021-09-17 12:56:07 -0300

    Componente Produto

---
+++ src/Components/Produto/Produto.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Produto = (props) => {
+  return (
+    <section>
+      <img src={props.src} alt={props.alt} />
+      {/* <h1>Tailored Jeans</h1>
+      <p class="price">$19.99</p>
+      <p>Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.</p>
+      <p><button>Add to Cart</button></p> */}
+    </section>
+  )
+}
+
+export default Produto
