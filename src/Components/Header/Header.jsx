4d3f83b390452b2fe4d4f92f87c359315084382a
Joao Porfirio <joaoprf54@gmail.com>
2021-09-17 12:34:08 -0300

    Initial commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <span>Ganhe 10 R$ de desconto no frete</span>
+      <nav>
+        <h1>Jordan Shoes</h1>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '../../assets/img/wallpaper.jpeg';
+const Main = () => {
+  return (
+    <section>
+      <main>
+        <img src={logo}/>
+        <h2>A melhor loja de Jordan</h2>
+      </main> 
+    </section>
+  )
+}
+
+export default Main
