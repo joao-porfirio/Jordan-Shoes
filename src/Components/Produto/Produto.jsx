9fbb051a4a0b4faf601033fca69b7bf001cd5395
Joao Porfirio <joaoprf54@gmail.com>
2021-09-19 17:39:36 -0300

    Ajustando os cards com animações utilizando React-Reveal

--- src/Components/Produto/Produto.jsx
+++ src/Components/Produto/Produto.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Slide from 'react-reveal/Slide';
+
 import jordan_1 from '../../assets/img/1.png'
 import jordan_2 from '../../assets/img/2.png'
 import jordan_3 from '../../assets/img/3.png'
@@ -10,47 +12,66 @@ import jordan_8 from '../../assets/img/8.png'
 
 const Produto = (props) => {
   return (
+   
     <section className="card">          
+     <Slide left>
       <article className="card__container">
         <picture className="card__thumbnail">
           <img src={jordan_1} alt="A banana that looks like a bird"/>
         </picture>   
       </article>
-      <article className="card__container">
-        <picture className="card__thumbnail">
-          <img src={jordan_2} alt="A banana that looks like a bird"/>
-        </picture>
-      </article>       
+      </Slide>
+
+      <Slide left big>
+        <article className="card__container">
+          <picture className="card__thumbnail">
+            <img src={jordan_2} alt="A banana that looks like a bird"/>
+          </picture>
+        </article>   
+      </Slide>  
+      
+      <Slide right big>
       <article className="card__container">
         <picture className="card__thumbnail">
           <img src={jordan_3} alt="A banana that looks like a bird"/>
         </picture>
-      </article>       
-      <article className="card__container">
-        <picture className="card__thumbnail">
-          <img src={jordan_4} alt="A banana that looks like a bird"/>
-        </picture>
-      </article>       
-      <article className="card__container">
-        <picture className="card__thumbnail">
-          <img src={jordan_5} alt="A banana that looks like a bird"/>
-        </picture>
-      </article>       
-      <article className="card__container">
-        <picture className="card__thumbnail">
-          <img src={jordan_6} alt="A banana that looks like a bird"/>
-        </picture>
-      </article>       
-      <article className="card__container">
-        <picture className="card__thumbnail">
-          <img src={jordan_7} alt="A banana that looks like a bird"/>
-        </picture>
-      </article>       
-      <article className="card__container">
-        <picture className="card__thumbnail">
-          <img src={jordan_8} alt="A banana that looks like a bird"/>
-        </picture>
-      </article>       
+      </article>   
+      </Slide>  
+      <Slide right>   
+        <article className="card__container">
+          <picture className="card__thumbnail">
+            <img src={jordan_4} alt="A banana that looks like a bird"/>
+          </picture>
+        </article>       
+      </Slide> 
+      <Slide left big>
+        <article className="card__container">
+          <picture className="card__thumbnail">
+            <img src={jordan_5} alt="A banana that looks like a bird"/>
+          </picture>
+        </article>   
+      </Slide>
+      <Slide left big>   
+        <article className="card__container">
+          <picture className="card__thumbnail">
+            <img src={jordan_6} alt="A banana that looks like a bird"/>
+          </picture>
+        </article> 
+      </Slide>   
+      <Slide right big>   
+        <article className="card__container">
+          <picture className="card__thumbnail">
+            <img src={jordan_7} alt="A banana that looks like a bird"/>
+          </picture>
+        </article>  
+      </Slide> 
+      <Slide right big>   
+        <article className="card__container">
+          <picture className="card__thumbnail">
+            <img src={jordan_8} alt="A banana that looks like a bird"/>
+          </picture>
+        </article>      
+      </Slide>   
     </section>
   )
 }
