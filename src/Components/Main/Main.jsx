6812411a1e6ba95b80a75ce05d78655580b2ed51
Joao Porfirio <joaoprf54@gmail.com>
2021-09-19 17:13:01 -0300

    Inserindo função e useEffect para uso da biblioteca TypedJS

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import banner from '../../assets/img/wallpaper.jpeg';
+import Typed from "typed.js";
+import React, {useEffect, useRef} from 'react'
 const Main = () => {
+
+  const el = useRef(null);
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["O tênis Jordan é fruto de uma velha e forte parceria entre a Nike e o jogador Michael Jordan."],
+      startDelay: 300,
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 100
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <main>
       <article className="article">
       </article> 
         <figcaption className="banner">
           <h2 className="banner__titulo">A melhor loja de Jordan</h2>
-          <p className="banner__texto">O tênis Jordan é fruto de uma velha e forte<br/> 
-          parceria entre a Nike e o jogador Michael Jordan.</p>
+          <p className="banner__texto" ref={el}></p>
         </figcaption>
     </main>
   )
