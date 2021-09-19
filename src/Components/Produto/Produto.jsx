6fcc52e59f8de98d848a7566bb6c3fc3409d51f9
Joao Porfirio <joaoprf54@gmail.com>
2021-09-18 23:01:46 -0300

    Utilização flexbox para alinhamentos dos cards

--- src/Components/Produto/Produto.jsx
+++ src/Components/Produto/Produto.jsx
@@ -1,13 +1,56 @@
 import React from 'react'
+import jordan_1 from '../../assets/img/1.png'
+import jordan_2 from '../../assets/img/2.png'
+import jordan_3 from '../../assets/img/3.png'
+import jordan_4 from '../../assets/img/4.png'
+import jordan_5 from '../../assets/img/5.png'
+import jordan_6 from '../../assets/img/6.png'
+import jordan_7 from '../../assets/img/7.png'
+import jordan_8 from '../../assets/img/8.png'
 
 const Produto = (props) => {
   return (
-    <section className="produtos">
-      <div className="produtos__container">
-        <div className="produtos__container-imagem">
-          <img src={props.src} alt={props.alt} />
-        </div>
-      </div>
+    <section className="cards">          
+      <article className="card">
+        <picture className="thumbnail">
+          <img src={jordan_1} alt="A banana that looks like a bird"/>
+        </picture>   
+      </article>
+      <article className="card">
+        <picture className="thumbnail">
+          <img src={jordan_2} alt="A banana that looks like a bird"/>
+        </picture>
+      </article>       
+      <article className="card">
+        <picture className="thumbnail">
+          <img src={jordan_3} alt="A banana that looks like a bird"/>
+        </picture>
+      </article>       
+      <article className="card">
+        <picture className="thumbnail">
+          <img src={jordan_4} alt="A banana that looks like a bird"/>
+        </picture>
+      </article>       
+      <article className="card">
+        <picture className="thumbnail">
+          <img src={jordan_5} alt="A banana that looks like a bird"/>
+        </picture>
+      </article>       
+      <article className="card">
+        <picture className="thumbnail">
+          <img src={jordan_6} alt="A banana that looks like a bird"/>
+        </picture>
+      </article>       
+      <article className="card">
+        <picture className="thumbnail">
+          <img src={jordan_7} alt="A banana that looks like a bird"/>
+        </picture>
+      </article>       
+      <article className="card">
+        <picture className="thumbnail">
+          <img src={jordan_8} alt="A banana that looks like a bird"/>
+        </picture>
+      </article>       
     </section>
   )
 }
