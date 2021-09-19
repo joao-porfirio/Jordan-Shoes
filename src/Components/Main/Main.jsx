2c0ecbe11eaa0f19b29203ded0969fffc60dfd16
Joao Porfirio <joaoprf54@gmail.com>
2021-09-18 22:16:29 -0300

    Ajustando tags semanticas

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -2,18 +2,15 @@ import React from 'react'
 import banner from '../../assets/img/wallpaper.jpeg';
 const Main = () => {
   return (
-    <section>
-      <main className="main">
-        <article className="main__info">
-          <img src={banner} alt="" className="main__banner" />
-        </article>
-        <aside className="main__banner-content">
-          <h2 className="main__banner-bord">A melhor loja de Jordan</h2>
-          <p className="main__banner-text">O tênis Jordan é fruto de uma velha e forte<br/> 
+    <main>
+      <article className="article">
+      </article> 
+        <figcaption className="banner">
+          <h2 className="banner__titulo">A melhor loja de Jordan</h2>
+          <p className="banner__texto">O tênis Jordan é fruto de uma velha e forte<br/> 
           parceria entre a Nike e o jogador Michael Jordan.</p>
-        </aside>
-      </main> 
-    </section>
+        </figcaption>
+    </main>
   )
 }
 
